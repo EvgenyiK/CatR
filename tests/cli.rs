d32e8fc7360f6cc3cd872b637953ba112a0e873a
oneil <evgenprogrammer@gmail.com>
2022-11-20 13:22:34 +0300

    init and fill tests

---
+++ tests/cli.rs
@@ -0,0 +1,13 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+use rand::{distributions::Alphanumeric, Rng};
+use std::error::Error;
+use std::fs;
+
+type TestResult = Result<(), Box<dyn Error>>;
+
+const PRG: &str = "catr";
+const EMPTY: &str = "tests/inputs/empty.txt";
+const FOX: &str = "tests/inputs/fox.txt";
+const SPIDERS: &str = "tests/inputs/spiders.txt";
+const BUSTLE: &str = "tests/inputs/the-bustle.txt";
